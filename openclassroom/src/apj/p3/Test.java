6bbf3a85bb7dcafa7e25b3b5cd3902dec1b1cdcb
bbsebb <sebastien.burckhardt@gmail.com>
2020-05-28 22:40:41 +0200

    ocr

--- openclassroom/src/apj/p3/Test.java
+++ openclassroom/src/apj/p3/Test.java
@@ -1,12 +1,31 @@
 package apj.p3;
 
+import java.lang.reflect.InvocationTargetException;
+
 import javax.swing.SwingUtilities;
 
 public class Test {
 
 	public static void main(String[] args) {
-		Fenetre f = new Fenetre();
-		
-	}
+		SwingUtilities.invokeLater(new Runnable() {
 
+			@Override
+			public void run() {
+				// Fenetre f = new Fenetre();
+				FenetreJText f2 = new FenetreJText();
+
+			}
+		});
+
+		SwingUtilities.invokeLater(new Runnable() {
+
+			@Override
+			public void run() {
+				// Fenetre f = new Fenetre();
+				FenetreKeyListener f3 = new FenetreKeyListener();
+
+			}
+		});
+
+	}
 }

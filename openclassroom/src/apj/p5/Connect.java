6c1430daedd63ba773708090e234b6b7ce9f4950
bbsebb <sebastien.burckhardt@gmail.com>
2020-10-15 22:09:17 +0200

    JBDC

---
+++ openclassroom/src/apj/p5/Connect.java
@@ -0,0 +1,24 @@
+package apj.p5;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+//CTRL + SHIFT + O pour générer les imports
+public class Connect {
+public static void main(String[] args) {      
+  try {
+    Class.forName("org.postgresql.Driver");
+    System.out.println("Driver O.K.");
+
+    String url = "jdbc:postgresql://localhost:5432/Ecole";
+    String user = "postgres";
+    String passwd = "ga9399ghr";
+
+    Connection conn = DriverManager.getConnection(url, user, passwd);
+    System.out.println("Connexion effective !");         
+       
+  } catch (Exception e) {
+    e.printStackTrace();
+  }      
+}
+}

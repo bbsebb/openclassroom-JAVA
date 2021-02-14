1a0bc86abac795a0ea9a259903eb7b841e8a16e4
bbsebb <sebastien.burckhardt@gmail.com>
2021-02-14 02:26:01 +0100

    tt

---
+++ openclassroom/src/apj/p3/ardoise/DessinPan.java
@@ -0,0 +1,48 @@
+package apj.p3.ardoise;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+import java.awt.event.MouseMotionListener;
+
+import javax.swing.JPanel;
+
+public class DessinPan extends JPanel{
+
+	private static final long serialVersionUID = 1L;
+	
+	int x = -15;
+	int y = -15;
+	
+	DessinPan () {
+		this.addMouseMotionListener(new MouseMotionListener() {
+			
+
+			public void mouseMoved(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+
+			public void mouseDragged(MouseEvent e) {
+				x = e.getX();
+				y = e.getY();
+				DessinPan.this.repaint();
+				
+			}
+		}); 
+		this.setBackground(Color.blue);
+	}
+	
+	public void paintComponent(Graphics gOld) {
+		Graphics2D g = (Graphics2D) gOld;
+		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
+		g.setColor(Color.black);
+		g.fillOval(x, y, 12, 12);
+	}
+	
+
+}

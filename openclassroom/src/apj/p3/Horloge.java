0a6c1dcf00e3e1cef616db40e956809d181ee222
bbsebb <sebastien.burckhardt@gmail.com>
2021-02-16 01:57:11 +0100

    tt

--- openclassroom/src/apj/p3/Horloge.java
+++ openclassroom/src/apj/p3/Horloge.java
@@ -1,6 +1,54 @@
 package apj.p3;
 
-public class Horloge 
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+public class Horloge implements Serializable
 {
 
+	private static final long serialVersionUID = -3048792873345356468L;
+	
+	PropertyChangeSupport changeSupport;
+	
+	public Horloge() {
+		changeSupport = new PropertyChangeSupport(this);
+	}
+	
+	LocalDateTime date;
+
+	public String getDate() {
+		return date.toString();
+	}
+	public void setDate(LocalDateTime date) {
+		LocalDateTime oldDate = this.date;
+		this.date = date;
+		changeSupport.firePropertyChange("temps",oldDate,date);
+	}
+	
+	public void run() {
+		while(true) {
+			this.setDate(LocalDateTime.now());
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	
+	public void addPropertyChangeListener(PropertyChangeListener listener) {
+		// TODO Auto-generated method stub
+		changeSupport.addPropertyChangeListener(listener);
+	}
+	
+
+	public void removePropertyChangeListener(PropertyChangeListener listener) {
+		// TODO Auto-generated method stub
+		changeSupport.removePropertyChangeListener(listener);
+	}
+	
 }
--- openclassroom/src/apj/p3/MaFenetre.java
+++ openclassroom/src/apj/p3/MaFenetre.java
@@ -1,34 +1,41 @@
 package apj.p3;
 
-import java.awt.Color;
-import java.awt.Graphics;
 import java.awt.HeadlessException;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 
 import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.JLabel;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.plaf.nimbus.NimbusLookAndFeel;
 
-public class MaFenetre extends JFrame {
+public class MaFenetre extends JFrame   {
 
 	private static final long serialVersionUID = 1L;
 
+	private JLabel label;
 	public MaFenetre(String title) throws HeadlessException {
 		super(title);
-
-		this.setLocationRelativeTo(null);
-		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		this.setSize(300, 400);
+		Horloge h = new Horloge();
+		h.addPropertyChangeListener((e) -> label.setText(e.getPropertyName() + " " + e.getNewValue()));
+		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		this.setSize(400,600);
+		
+		label = new JLabel();
+		this.getContentPane().add(label);
+	
 		this.setVisible(true);
+		h.run();
 		
 	}
 
 	public static void main(String[] args) throws UnsupportedLookAndFeelException {
 		UIManager.setLookAndFeel(new NimbusLookAndFeel());
 		JFrame f = new MaFenetre("ma fenetre");
-
 	}
+
+
 	
 	
 

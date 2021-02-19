d7366e1b1f93fa7c7e68dcec79f1ca973c711cc0
bbsebb <sebastien.burckhardt@gmail.com>
2021-02-19 13:04:18 +0100

    tt

--- openclassroom/src/apj/p3/calculatrice/AbstractController.java
+++ openclassroom/src/apj/p3/calculatrice/AbstractController.java
@@ -2,6 +2,43 @@ package apj.p3.calculatrice;
 
 public abstract class AbstractController {
 	
-	public abstract String conroleChiffre () ;
+	String chiffre;
+	String op;
+	AbstractModel model;
+	
+	public abstract String controleChiffre () ;
+	
+	public abstract boolean controleOp();
+	
+	public abstract boolean controleCancel();
+	
+	public abstract boolean controleEgal();
+
+	protected String getChiffre() {
+		return chiffre;
+	}
+
+	protected void setChiffre(String chiffre) {
+		this.chiffre = chiffre;
+	}
+
+	protected String getOp() {
+		return op;
+	}
+
+	protected void setOp(String op) {
+		this.op = op;
+	}
+
+	protected AbstractModel getModel() {
+		return model;
+	}
+
+	protected void setModel(AbstractModel model) {
+		this.model = model;
+	}
+	
+	
+	
 
 }
--- openclassroom/src/apj/p3/calculatrice/AbstractModel.java
+++ openclassroom/src/apj/p3/calculatrice/AbstractModel.java
@@ -1,5 +1,36 @@
 package apj.p3.calculatrice;
 
-public abstract class AbstractModel {
+import java.beans.PropertyChangeSupport;
 
+public abstract class AbstractModel {
+	int rslt;
+	PropertyChangeSupport support;
+	
+	AbstractModel() {
+		support = new PropertyChangeSupport(this);
+	}
+	
+	public abstract boolean operationAdditionner ();
+	public abstract boolean operationSoustraire ();
+	public abstract boolean operationDiviser ();
+	public abstract boolean operationMultiplier ();
+	public abstract boolean reset ();
+	
+	protected int getRslt() {
+		return rslt;
+	}
+	protected void setRslt(int rslt) {
+		this.rslt = rslt;
+	}
+
+	protected PropertyChangeSupport getSupport() {
+		return support;
+	}
+
+	protected void setSupport(PropertyChangeSupport support) {
+		this.support = support;
+	}
+	
+	
+	
 }

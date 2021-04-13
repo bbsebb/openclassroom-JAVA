1c7d108559902b5e5b95b6eb9fbe49cfdd8327f4
bbsebb <sebastien.burckhardt@gmail.com>
2021-04-13 20:22:34 +0200

    tt

--- openclassroom/src/Main.java
+++ openclassroom/src/Main.java
@@ -1,12 +1,18 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		
-		for(int i=0; i< 100; i++) {
-			System.out.println("Salut, Laurie !!");
-		}
+	JFrame g = new JFrame("Test");
+	g.setSize(400, 400);
+	g.setLocationRelativeTo(null);
+	BeanPropertiesTable BeanPorperties = new BeanPropertiesTable();
+	BeanPorperties.setInstance((Object) new JButton("test"));
+	g.getContentPane().add(BeanPorperties);
+	g.setVisible(true);
 		
 	}
 
